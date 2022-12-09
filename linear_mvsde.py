0106e926f8811e6ba9f7d1c1f74f5c4e5b7fe1c0
Louis Sharrock <louis.sharrock@bristol.ac.uk>
2022-12-09 12:17:39 +0000

    add linear ips simulator

--- linear_mvsde.py
+++ linear_mvsde.py
@@ -49,6 +49,46 @@ def linear_mvsde_sim_func(N=1, T=100, alpha=0.5, beta=0.1, sigma=1, x0=1, dt=0.1
 #########################
 
 
+################################
+#### LINEAR IPS SIMULATOR ####
+################################
+
+## Inputs:
+## -> N (number of particles)
+## -> T (length of simulation)
+## -> alpha (parameter for confinement potential)
+## -> beta (parameter for interaction potential)
+## -> sigma (noise magnitude)
+## -> x0  (initial value)
+## -> dt (time step)
+## -> seed (random seed)
+## Outputs:
+## -> xt_1 (a single particle)
+
+def linear_ips_sim_func(N=10, T=100, alpha=0.5, beta=0.1, sigma=1, x0=1, dt=0.1, seed=1):
+    ## set random seed
+    np.random.seed(seed)
+
+    ## number of time steps
+    nt = int(np.round(T / dt))
+
+    ## initialise xt
+    xt = np.zeros((nt + 1, N))
+    xt[0, :] = x0
+
+    ## brownian motion
+    dwt = np.sqrt(dt) * np.random.randn(nt + 1, N)
+
+    ## simulate
+    for i in range(0, nt):
+        xt[i + 1, :] = xt[i, :] - alpha * xt[i, :]* dt - beta * (xt[i, :] - np.mean(xt[i, :])) * dt + sigma * dwt[i, :]
+
+    ## output the first particle
+    return xt[:,0]
+
+#########################
+
+
 def linear_mvsde_online_est_one(xt, theta0, theta_true, est_theta1=True, est_theta2=True, gamma=0.01, sigma=1):
 
     ## number of time steps
@@ -141,6 +181,7 @@ def linear_mvsde_online_est_two(xt, theta0, theta_true, est_theta1=True, est_the
 
 
 def linear_mvsde_online_est_one_particle_approx(xt, theta0, theta_true, est_theta1, est_theta2, gamma, sigma=1, N=2):
+
     ## number of time steps
     nt = xt.shape[0] - 1
 
@@ -265,12 +306,13 @@ if __name__ == '__main__':
 
     # simulation parameters
     N_obs = 1
+    N_par = 100
     T = 20000
     dt = 0.1
     alpha = 1.5
     beta = 0.7
     sigma = 1
-    seeds = range(20)
+    seeds = range(10)
 
     nt = round(T/dt)
     t = [i * dt for i in range(nt+1)]
@@ -279,88 +321,97 @@ if __name__ == '__main__':
     gamma = 0.005
     theta0 = np.array([2.0, 0.2])
     theta_true = np.array([alpha, beta])
-    est_theta1 = True
-    est_theta2 = False
+    est_theta1 = False
+    est_theta2 = True
     N_est = 2
 
     # plotting
     plot_each_run = False
     plot_mean_run = True
 
+    # observations
+    observations = ['linear_mvsde','linear_ips']
+
     # output
     save_plots = True
-    save_root = "results/linear_mvsde/"
 
-    all_thetat_est1 = np.zeros((nt+1, 2, len(seeds)))
-    all_thetat_est2 = np.zeros((nt + 1, 2, len(seeds)))
-    all_thetat_est1_approx = np.zeros((nt + 1, 2, len(seeds)))
-    all_thetat_est2_approx = np.zeros((nt + 1, 2, len(seeds)))
+    for obs in observations:
 
-    for idx, seed in enumerate(seeds):
+        save_root = "results/" + obs + "/"
 
-        print(seed)
+        all_thetat_est1 = np.zeros((nt+1, 2, len(seeds)))
+        all_thetat_est2 = np.zeros((nt + 1, 2, len(seeds)))
+        all_thetat_est1_approx = np.zeros((nt + 1, 2, len(seeds)))
+        all_thetat_est2_approx = np.zeros((nt + 1, 2, len(seeds)))
 
-        # simulate mvsde
-        x0 = np.random.randn(1)
-        xt = linear_mvsde_sim_func(N_obs, T, alpha, beta, sigma, x0, dt, seed)
+        for idx, seed in enumerate(seeds):
 
-        thetat_est1 = linear_mvsde_online_est_one(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma)
-        thetat_est2 = linear_mvsde_online_est_two(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma)
-        thetat_est1_approx = linear_mvsde_online_est_one_particle_approx(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma, N_est)
-        thetat_est2_approx = linear_mvsde_online_est_two_particle_approx(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma, N_est)
+            print(seed)
 
-        all_thetat_est1[:,:,idx] = thetat_est1
-        all_thetat_est2[:,:,idx] = thetat_est2
-        all_thetat_est1_approx[:, :, idx] = thetat_est1_approx
-        all_thetat_est2_approx[:, :, idx] = thetat_est2_approx
+            # simulate mvsde
+            x0 = np.random.randn(1)
+            if obs == "linear_mvsde":
+                xt = linear_mvsde_sim_func(N_obs, T, alpha, beta, sigma, x0, dt, seed)
+            if obs == "linear_ips":
+                xt = linear_ips_sim_func(N_par, T, alpha, beta, sigma, x0, dt, seed)
 
-        if plot_each_run:
+            thetat_est1 = linear_mvsde_online_est_one(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma)
+            thetat_est2 = linear_mvsde_online_est_two(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma)
+            thetat_est1_approx = linear_mvsde_online_est_one_particle_approx(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma, N_est)
+            thetat_est2_approx = linear_mvsde_online_est_two_particle_approx(xt.copy(), theta0, theta_true, est_theta1, est_theta2, gamma, sigma, N_est)
+
+            all_thetat_est1[:,:,idx] = thetat_est1
+            all_thetat_est2[:,:,idx] = thetat_est2
+            all_thetat_est1_approx[:, :, idx] = thetat_est1_approx
+            all_thetat_est2_approx[:, :, idx] = thetat_est2_approx
+
+            if plot_each_run:
+                if est_theta1 is True and est_theta2 is False:
+                    plt.plot(t, thetat_est1[:,0], label=r"$\theta_{t,1}$ (Estimator 1)")
+                    plt.plot(t, thetat_est2[:,0], label=r"$\theta_{t,1}$ (Estimator 2)")
+                    plt.plot(t, thetat_est1_approx[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 1, Approx)")
+                    plt.plot(t, thetat_est2_approx[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 2, Approx)")
+                    plt.axhline(y=alpha, linestyle="--")
+                    plt.legend()
+                    plt.show()
+                if est_theta1 is False and est_theta2 is True:
+                    plt.plot(t, thetat_est1[:,1], label=r"$\theta_{t,2}^N$ (Estimator 1)")
+                    plt.plot(t, thetat_est2[:,1], label=r"$\theta_{t,2}^N$ (Estimator 2)")
+                    plt.plot(t, thetat_est1_approx[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 1, Approx)")
+                    plt.plot(t, thetat_est2_approx[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 2, Approx)")
+                    plt.axhline(y=beta,linestyle="--")
+                    plt.legend()
+                    plt.show()
+
+        if plot_mean_run:
             if est_theta1 is True and est_theta2 is False:
-                plt.plot(t, thetat_est1[:,0], label=r"$\theta_{t,1}$ (Estimator 1)")
-                plt.plot(t, thetat_est2[:,0], label=r"$\theta_{t,1}$ (Estimator 2)")
-                plt.plot(t, thetat_est1_approx[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 1, Approx)")
-                plt.plot(t, thetat_est2_approx[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 2, Approx)")
-                plt.axhline(y=alpha, linestyle="--")
+                plt.plot(t, np.mean(all_thetat_est1, 2)[:,0], label=r"$\theta_{t,1}$ (Estimator 1)")
+                plt.plot(t, np.mean(all_thetat_est2, 2)[:,0], label=r"$\theta_{t,1}$ (Estimator 2)")
+                plt.plot(t, np.mean(all_thetat_est1_approx, 2)[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 1, Approx)")
+                plt.plot(t, np.mean(all_thetat_est2_approx, 2)[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 2, Approx)")
+                plt.axhline(y=alpha, linestyle="--", color="black")
                 plt.legend()
+                if save_plots:
+                    plt.savefig(save_root + "alpha_est_all.eps", dpi=300)
                 plt.show()
-            if est_theta1 is False and est_theta2 is True:
-                plt.plot(t, thetat_est1[:,1], label=r"$\theta_{t,2}^N$ (Estimator 1)")
-                plt.plot(t, thetat_est2[:,1], label=r"$\theta_{t,2}^N$ (Estimator 2)")
-                plt.plot(t, thetat_est1_approx[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 1, Approx)")
-                plt.plot(t, thetat_est2_approx[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 2, Approx)")
-                plt.axhline(y=beta,linestyle="--")
+            elif est_theta1 is False and est_theta2 is True:
+                plt.plot(t, np.mean(all_thetat_est1,2)[:,1], label=r"$\theta_{t,2}$ (Estimator 1)")
+                plt.plot(t, np.mean(all_thetat_est2,2)[:,1], label=r"$\theta_{t,2}$ (Estimator 2)")
+                plt.plot(t, np.mean(all_thetat_est1_approx, 2)[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 1, Approx)")
+                plt.plot(t, np.mean(all_thetat_est2_approx, 2)[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 2, Approx)")
+                plt.axhline(y=beta, linestyle="--", color="black")
                 plt.legend()
+                if save_plots:
+                    plt.savefig(save_root + "beta_est_all.eps", dpi=300)
                 plt.show()
-
-    if plot_mean_run:
-        if est_theta1 is True and est_theta2 is False:
-            plt.plot(t, np.mean(all_thetat_est1, 2)[:,0], label=r"$\theta_{t,1}$ (Estimator 1)")
-            plt.plot(t, np.mean(all_thetat_est2, 2)[:,0], label=r"$\theta_{t,1}$ (Estimator 2)")
-            plt.plot(t, np.mean(all_thetat_est1_approx, 2)[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 1, Approx)")
-            plt.plot(t, np.mean(all_thetat_est2_approx, 2)[:, 0], label=r"$\theta_{t,1}^N$ (Estimator 2, Approx)")
-            plt.axhline(y=alpha, linestyle="--", color="black")
-            plt.legend()
-            if save_plots:
-                plt.savefig(save_root + "alpha_est_all.eps", dpi=300)
-            plt.show()
-        elif est_theta1 is False and est_theta2 is True:
-            plt.plot(t, np.mean(all_thetat_est1,2)[:,1], label=r"$\theta_{t,2}$ (Estimator 1)")
-            plt.plot(t, np.mean(all_thetat_est2,2)[:,1], label=r"$\theta_{t,2}$ (Estimator 2)")
-            plt.plot(t, np.mean(all_thetat_est1_approx, 2)[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 1, Approx)")
-            plt.plot(t, np.mean(all_thetat_est2_approx, 2)[:, 1], label=r"$\theta_{t,2}^N$ (Estimator 2, Approx)")
-            plt.axhline(y=beta, linestyle="--", color="black")
-            plt.legend()
-            if save_plots:
-                plt.savefig(save_root + "beta_est_all.eps", dpi=300)
-            plt.show()
-        elif est_theta1 is True and est_theta2 is True:
-            plt.plot(t, np.mean(all_thetat_est1, 2), label=r"$\theta_{t}$ (Estimator 1)")
-            plt.plot(t, np.mean(all_thetat_est2, 2), label=r"$\theta_{t}$ (Estimator 2)")
-            plt.plot(t, np.mean(all_thetat_est1_approx, 2), label=r"$\theta_{t}^N$ (Estimator 1, Approx)")
-            plt.plot(t, np.mean(all_thetat_est2_approx, 2), label=r"$\theta_{t}^N$ (Estimator 2, Approx)")
-            plt.axhline(y=alpha, linestyle="--", color="black")
-            plt.axhline(y=beta, linestyle="--", color="black")
-            plt.legend()
-            if save_plots:
-                plt.savefig(save_root + "alpha_beta_est_all.eps", dpi=300)
-            plt.show()
+            elif est_theta1 is True and est_theta2 is True:
+                plt.plot(t, np.mean(all_thetat_est1, 2), label=r"$\theta_{t}$ (Estimator 1)")
+                plt.plot(t, np.mean(all_thetat_est2, 2), label=r"$\theta_{t}$ (Estimator 2)")
+                plt.plot(t, np.mean(all_thetat_est1_approx, 2), label=r"$\theta_{t}^N$ (Estimator 1, Approx)")
+                plt.plot(t, np.mean(all_thetat_est2_approx, 2), label=r"$\theta_{t}^N$ (Estimator 2, Approx)")
+                plt.axhline(y=alpha, linestyle="--", color="black")
+                plt.axhline(y=beta, linestyle="--", color="black")
+                plt.legend()
+                if save_plots:
+                    plt.savefig(save_root + "alpha_beta_est_all.eps", dpi=300)
+                plt.show()
